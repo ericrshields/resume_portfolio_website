5d57ef3fefa02becac3ed67a4f401edfb16254ed
Eric Shields <ershield@coldcandor.com>
2023-05-25 17:23:48 -0700

    Add gh-pages and config to make site deployable on github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    assetPrefix: isProd ? '/resume_portfolio_website/' : '',
+    basePath: isProd ? '/resume_portfolio_website' : '',
+    images: {
+        unoptimized: true,
+    },
+    output: "export",
+}
 
 module.exports = nextConfig
