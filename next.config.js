a8a255575420ab03f7e03f1867927c3fb7ac3f23
Eric Shields <ershield@coldcandor.com>
2023-05-26 16:04:23 -0700

    Remove prefixes now that a custom domain is in use

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-    assetPrefix: isProd ? '/resume_portfolio_website/' : '',
-    basePath: isProd ? '/resume_portfolio_website' : '',
     images: {
         unoptimized: true,
     },
