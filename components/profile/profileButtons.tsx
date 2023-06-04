134b47545137ed78cfe58eae1f8c517349f55a33
Eric Shields <ershield@coldcandor.com>
2023-06-03 19:47:39 -0700

    Remove last references to Tomar and prep for deploy

--- components/profile/profileButtons.tsx
+++ components/profile/profileButtons.tsx
@@ -1,10 +1,9 @@
-import { strings } from "@/lib/strings.enUS";
+import { links, strings } from "@/lib/strings.enUS";
 
-import { RiWhatsappLine, RiMessengerLine } from "react-icons/ri"
+import { RiMailLine } from "react-icons/ri"
 
 export default function profileButtons() {
 
-    // TODO: Replace social links with ones I actually care about
     return (
         <div className="profile__buttons">
             <a download="" href="/eric-shields-resume.pdf" className="button">
@@ -12,11 +11,8 @@ export default function profileButtons() {
             </a>
 
             <div className="profile__buttons-small">
-                <a href="https://api.whatsapp.com/send?phone=+919999999999&text=Hello, more information!" target="_blank" className="button button__small button__gray">
-                    <RiWhatsappLine />
-                </a>
-                <a href="https://m.me/vaibhav" target="_blank" className="button button__small button__gray">
-                    <RiMessengerLine />
+                <a href={links.email} target="_blank" className="button button__small button__gray">
+                    <RiMailLine />
                 </a>
             </div>
         </div>
