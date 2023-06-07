96569e823748d10b335e0b486b92e77eab6c6759
Eric Shields <ershield@coldcandor.com>
2023-06-06 22:49:58 -0700

    Fix theme not applying from localStorage on load; Remove localStorage fallback - not needed with SSR off

--- components/themeSwitch.tsx
+++ components/themeSwitch.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import { RiMoonLine, RiSunLine } from "react-icons/ri";
 
 const MAGIC_STRINGS = {
@@ -8,36 +8,25 @@ const MAGIC_STRINGS = {
 }
 
 export default function ThemeSwitch() {
-    // Create empty fallback to prevent runtime "localStorage is not defined" error
-    const _localStorage = typeof window !== 'undefined' ? localStorage : {
-        length: 0,
-        clear() {},
-        getItem(key: string): string | null { return ""; },
-        key(index: number): string | null { return ""; },
-        removeItem(key: string) {},
-        setItem(key: string, value: string) {},
-    };
-
     /**
      * Theme handling
      */
-    const initialTheme = _localStorage.getItem(MAGIC_STRINGS.SELECTED_THEME) || MAGIC_STRINGS.DARK_THEME;
+    const initialTheme = localStorage.getItem(MAGIC_STRINGS.SELECTED_THEME) || MAGIC_STRINGS.DARK_THEME;
     const [ selectedTheme, setSelectedTheme ] = useState(initialTheme);
-    // TODO: Update this to use the same style as tabHandler?
-    const themeHandler: MouseEventHandler<HTMLDivElement> = () => {
-        if (selectedTheme === MAGIC_STRINGS.DARK_THEME) {
-            setSelectedTheme(MAGIC_STRINGS.LIGHT_THEME);
-            _localStorage.setItem(MAGIC_STRINGS.SELECTED_THEME, MAGIC_STRINGS.LIGHT_THEME);
 
-            // Updating the body class requires a different approach, since moving this code into layout.tsx
-            // would require making the core layout a Client component, which causes other issues
-            document.body.classList.replace(MAGIC_STRINGS.DARK_THEME, MAGIC_STRINGS.LIGHT_THEME);
-        } else {
-            setSelectedTheme(MAGIC_STRINGS.DARK_THEME);
-            _localStorage.setItem(MAGIC_STRINGS.SELECTED_THEME, MAGIC_STRINGS.DARK_THEME);
-            document.body.classList.replace(MAGIC_STRINGS.LIGHT_THEME, MAGIC_STRINGS.DARK_THEME);
-        }
+    const themeHandler: MouseEventHandler<HTMLDivElement> = () => {
+        setSelectedTheme(
+            selectedTheme === MAGIC_STRINGS.DARK_THEME
+                ? MAGIC_STRINGS.LIGHT_THEME
+                : MAGIC_STRINGS.DARK_THEME
+        );
     }
+
+    useEffect(() => {
+        localStorage.setItem(MAGIC_STRINGS.SELECTED_THEME, selectedTheme)
+        document.body.classList.remove(MAGIC_STRINGS.DARK_THEME, MAGIC_STRINGS.LIGHT_THEME);
+        document.body.classList.add(selectedTheme);
+    }, [selectedTheme])
 
     return (
         <div className={"change-theme"} onClick={themeHandler}>
